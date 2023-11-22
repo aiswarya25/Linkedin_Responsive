5c8ae43f9e78f6aec6446ee6b4e2ade0b2e3a503
aiswarya25 <aiswaryapbindhu3@gmail.com>
2023-11-22 14:37:28 +0530

    Added changes on buttons

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let profileMenu = document.getElementById("profileMenu");
 function toggleMenu(){
     profileMenu.classList.toggle("open-menu");
 }
+
+
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity")
+}
