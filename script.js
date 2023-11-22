9f05c23e1c38d09de9c97dd3468e9c844baab07d
aiswarya25 <aiswaryapbindhu3@gmail.com>
2023-11-22 14:51:28 +0530

    Completed changes

--- script.js
+++ script.js
@@ -10,4 +10,10 @@ let moreLink = document.getElementById("showMoreLink");
 
 function toggleActivity(){
     sideActivity.classList.toggle("open-activity");
+
+    if(sideActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show less <b>-</b>";
+    }else{
+        moreLink.innerHTML = "Show less <b>+</b>";
+    }
 }
